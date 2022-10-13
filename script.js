bf18e989435c48fbff866aad1bfcbbd96f19e1e1
SaptakCr7 <saptaksinha13898@gmail.com>
2022-10-13 12:10:12 +0530

    rich text editor

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let alignButtons = document.querySelectorAll(".align");
 let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
-
 //List of fontlist
 let fontList = [
   "Arial",
@@ -20,7 +19,6 @@ let fontList = [
   "cursive",
   "Poppins",
 ];
-
 //Initial Settings
 const initializer = () => {
   //function calls for highlighting buttons
@@ -29,7 +27,6 @@ const initializer = () => {
   highlighter(spacingButtons, true);
   highlighter(formatButtons, false);
   highlighter(scriptButtons, true);
-
   //create options for font names
   fontList.map((value) => {
     let option = document.createElement("option");
@@ -37,7 +34,6 @@ const initializer = () => {
     option.innerHTML = value;
     fontName.appendChild(option);
   });
-
   //fontSize allows only till 7
   for (let i = 1; i <= 7; i++) {
     let option = document.createElement("option");
@@ -45,33 +41,37 @@ const initializer = () => {
     option.innerHTML = i;
     fontSizeRef.appendChild(option);
   }
-
   //default size
-  fontSizeRef.value = 5;
+  fontSizeRef.value = 3;
 };
-
 //main logic
 const modifyText = (command, defaultUi, value) => {
   //execCommand executes command on selected text
   document.execCommand(command, defaultUi, value);
 };
-
 //For basic operations which don't need value parameter
 optionsButtons.forEach((button) => {
   button.addEventListener("click", () => {
     modifyText(button.id, false, null);
   });
 });
-
 //options that require value parameter (e.g colors, fonts)
 advancedOptionButton.forEach((button) => {
   button.addEventListener("change", () => {
     modifyText(button.id, false, button.value);
   });
 });
-
 //link
-
+linkButton.addEventListener("click", () => {
+  let userLink = prompt("Enter a URL");
+  //if link has http then pass directly else add https
+  if (/http/i.test(userLink)) {
+    modifyText(linkButton.id, false, userLink);
+  } else {
+    userLink = "http://" + userLink;
+    modifyText(linkButton.id, false, userLink);
+  }
+});
 //Highlight clicked button
 const highlighter = (className, needsRemoval) => {
   className.forEach((button) => {
@@ -79,12 +79,10 @@ const highlighter = (className, needsRemoval) => {
       //needsRemoval = true means only one button should be highlight and other would be normal
       if (needsRemoval) {
         let alreadyActive = false;
-
         //If currently clicked button is already active
         if (button.classList.contains("active")) {
           alreadyActive = true;
         }
-
         //Remove highlight from other buttons
         highlighterRemover(className);
         if (!alreadyActive) {
@@ -98,12 +96,12 @@ const highlighter = (className, needsRemoval) => {
     });
   });
 };
-
 const highlighterRemover = (className) => {
   className.forEach((button) => {
     button.classList.remove("active");
   });
 };
+window.onload = initializer();
 
 var noPrintElements = [];
 
